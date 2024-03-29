4f5b063d2a27ba73c212608ce329471619bbc708
OsamabinAdnan <binadnanosama@gmail.com>
2024-03-29 23:06:37 +0500

    Project02a_ATM (without assignment changes)

---
+++ Project02a_ATM/main.js
@@ -0,0 +1,38 @@
+import inquirer from "inquirer";
+let myBalance = 10000; // in $
+let myPin = 1234;
+let pinAnswer = await inquirer.prompt([
+    {
+        name: "pin",
+        message: "Enter you 4 digit pin",
+        type: "number",
+    }
+]);
+if (pinAnswer.pin === myPin) {
+    console.log("Wellcome! You pincode in valid");
+    let operationAns = await inquirer.prompt([
+        {
+            name: "operation",
+            message: "Select 1 option from below list",
+            type: "list",
+            choices: ["Withdraw", "Check Balance"]
+        }
+    ]);
+    if (operationAns.operation === "Withdraw") {
+        let amountAns = await inquirer.prompt([
+            {
+                name: "amount",
+                message: "Enter your amount",
+                type: "number",
+            }
+        ]);
+        myBalance -= amountAns.amount;
+        console.log("Your remaining balance is: " + myBalance);
+    }
+    else if (operationAns.operation === "Check Balance") {
+        console.log("Your balance is: " + myBalance);
+    }
+}
+else {
+    console.log("Invalid pin code");
+}
